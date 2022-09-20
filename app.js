2d362c8b1bdddbdafeb8ecd8b40aab1b65ad35d1
flash-of-genius <amhdaar@gmail.com>
2022-09-20 11:40:04 +0100

    032 Count Unique Values Solution

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-// write a function called sumZero which accepts a sorted array of integers. the function should find the first pair where the sum 0.
-
-function sumZero(arr) {
-  let left = 0;
-  let right = arr.length - 1;
-
-  while (left < right) {
-    let sum = arr[left] + arr[right];
-    if (sum === 0) {
-      return [arr[left], arr[right]];
-    } else if (sum > 0) {
-      right--;
-    } else {
-      left++;
+// implement a function called countUniqueValues, which accept a sorted array, and count the unique values in the array.
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
     }
   }
+  return i + 1;
 }
-console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5]));
-
-// function sumZero(arr) {
-//   for (let val1 of arr) {
-//     let val2 = val1 + 1;
-//     for (let val2 of arr) {
-//       if (val1 + val2 === 0) {
-//         return [val1, val2];
-//       }
-//     }
-//   }
-// }
-// console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5]));
+console.log(countUniqueValues([1, 1, 1, 2]));
