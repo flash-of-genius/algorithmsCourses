188579e591aa78c87752635befed0ed4df99ac19
flash-of-genius <amhdaar@gmail.com>
2022-09-20 10:31:35 +0100

    030 Multiple Pointers Pattern

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-// ANAGRAMS: given two strings, write a function to determine if the second string is an anagram of the first. an anagram is a word , phrase formed by rearranging the letters o another: cinema: iceman
+// write a function called sumZero which accepts a sorted array of integers. the function should find the first pair where the sum 0.
 
-function validAnagram(first, second) {
-  if (first.length !== second.length) {
-    return false;
-  }
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
 
-  const lookup = {};
-
-  for (let letter of first) {
-    // if letter exists, increment otherwise set to 1
-    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
-  }
-
-  for (let letter of second) {
-    // can't find letter or letter is zero then its not an anagram
-    if (!lookup[letter]) {
-      return false;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
     } else {
-      lookup[letter] -= 1;
+      left++;
     }
   }
-  return true;
 }
+console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5]));
 
-console.log(validAnagram("amaw", "awam"));
+// function sumZero(arr) {
+//   for (let val1 of arr) {
+//     let val2 = val1 + 1;
+//     for (let val2 of arr) {
+//       if (val1 + val2 === 0) {
+//         return [val1, val2];
+//       }
+//     }
+//   }
+// }
+// console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5]));
