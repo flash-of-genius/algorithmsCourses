b6e4b67c183f3721bc39084a5b29802bbed69fd2
flash-of-genius <amhdaar@gmail.com>
2022-09-21 12:38:13 +0100

    043 Our First Recursive Function

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-// Given a sorted arrayn of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located.
+function countDown(num) {
+  if (num <= 0) {
+    console.log('All Done!!!');
+    return;
+  }
+  console.log(num);
+  num--;
+  countDown(num);
+}
 
-console.log(search([1, 2, 8, 15, 40, 60, 50, 122, 46, 22, 88, 48], 88));
-
-// function search(arr, val) {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] === val) {
-//       return i;
-//     }
+// function countDown(num) {
+//   for (let i = num; i > 0; i--) {
+//     console.log(i);
 //   }
-//   return -1;
+//   console.log('All Done!!');
 // }
+console.log(countDown(5));
