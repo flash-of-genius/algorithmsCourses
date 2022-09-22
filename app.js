fd96deb2335550fc3924dced6dd60133b62fc63a
flash-of-genius <amhdaar@gmail.com>
2022-09-22 12:54:47 +0100

    062 Binary Search Solution

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-// linear search
-
-function search(arr, val) {
-  if (arr.includes(val)) {
-    return arr.indexOf(val);
-  } else {
-    return -1;
+// Original Solution
+function binarySearch(arr, elem) {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
   }
+  if (arr[middle] === elem) {
+    return middle;
+  }
+  return -1;
 }
-console.log(search([5, 2, 6, 2, 2, 7], 6));
+
+// Refactored Version
+function binarySearch(arr, elem) {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === elem ? middle : -1;
+}
+
+binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103);
