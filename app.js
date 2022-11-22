cf00c768b08114e672b96a61a896b26bc19623f6
flash-of-genius <amhdaar@gmail.com>
2022-11-22 12:35:25 +0100

    10 - Power of Two

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-// Prime Number
-
-function isPrime(n) {
-  if (n < 2) return false;
-  for (let i = 2; i < n; i++) {
-    if (n % i === 0) {
-      return false;
-    }
-  }
-  return true;
+// // Power of Two
+function isPowerOfTwo(n) {
+  if (n === 1) return true;
+  if (n % 2 !== 0) return false;
+  return isPowerOfTwo(n / 2);
 }
-console.log(isPrime(7));
+console.log(isPowerOfTwo(3));
 
-function isPrime1(n) {
-  if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) {
+function isPowerOfTwo1(n) {
+  if (n < 1) {
     return false;
   }
-  let m = Math.sqrt(n);
-
-  for (let i = 2; i <= m; i++) {
-    if (n % i === 0) return false;
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n = n / 2;
   }
   return true;
 }
-console.log(isPrime1(7)); //Output: True
-console.log(isPrime1(6)); //Output: False
+
+console.log(isPowerOfTwo1(3));
+
+function isPowerOfTwoBitWise(n) {
+  if (n < 1) {
+    return false;
+  }
+  return (n & (n - 1)) === 0;
+}
+
+console.log(isPowerOfTwoBitWise(8));
