4175893e3f1c7e13ecc4107145a885c07720217a
flash-of-genius <amhdaar@gmail.com>
2022-11-22 08:37:38 +0100

    8 - Factorial of a Numbe

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
-// Fibonacci sequence
-function Fibonacci(n) {
-  let arr = [0, 1];
-  for (let i = 2; i < n; i++) {
-    arr.push(arr[i - 1] + arr[i - 2]);
-  }
-  return arr;
+// Factorial of a number
+// with recursion
+function factorialize1(n) {
+  if (n < 0) return -1;
+  else if (n == 0) return 1;
+  else return n * factorialize1(n - 1);
 }
-console.log('Fibonacci(3)');
-console.log(Fibonacci(3));
+console.log(factorialize1(5));
 
-function fibonacci(n) {
-  const fib = [0, 1];
-  for (let i = 2; i < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
+// with a while loop
+function factorialize2(n) {
+  let result = n;
+  if (n === 0 || n === 1) return 1;
+  while (n > 1) {
+    n--;
+    result *= n;
   }
-  return fib;
+  return result;
+}
+console.log(factorialize2(10));
+
+//with a for loop
+function factorialize3(n) {
+  if (n === 0 || n === 1) return 1;
+
+  for (let i = n - 1; i > 1; i--) {
+    n *= i;
+  }
+  return n;
 }
 
-console.log('fibonacci(3)');
-console.log(fibonacci(3));
+console.log(factorialize3(15));
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+}
+console.log(factorial(20));
